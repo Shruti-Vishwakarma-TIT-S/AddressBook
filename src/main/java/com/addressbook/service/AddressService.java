d95dc3f5f6e31f62c4d736535ccd5ea78161ba57
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-03-03 20:20:34 +0530

    [Shruti] Add. Add Service Layer to store address book data

--- src/main/java/com/addressbook/service/AddressService.java
+++ src/main/java/com/addressbook/service/AddressService.java
@@ -1,46 +1,57 @@
 package com.addressbook.service;
 
 import com.addressbook.model.AddressModel;
-import com.addressbook.repository.AddressRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AddressService {
 
-    // UC2-ServiceLayer-------------------------------------------
-    @Autowired
-    private AddressRepository addressRepository;
+    // In-memory data store (List)
+    private List<AddressModel> addressList = new ArrayList<>();
 
     // Get all addresses
     public List<AddressModel> getAllAddresses() {
-        return addressRepository.findAll();
+        return addressList;
     }
 
     // Get an address by ID
     public Optional<AddressModel> getAddressById(Long id) {
-        return addressRepository.findById(id);
+        return addressList.stream()
+                .filter(address -> address.getId().equals(id))
+                .findFirst();
     }
 
     // Add a new address
     public AddressModel addAddress(AddressModel address) {
-        return addressRepository.save(address);
+        // Simulate auto-incrementing ID
+        long nextId = addressList.size() + 1;
+        address.setId(nextId);
+        addressList.add(address);
+        return address;
     }
 
-    //UC2- RestController-------------------------------------------
     // Update an existing address
     public AddressModel updateAddress(Long id, AddressModel address) {
-        address.setId(id);
-        return addressRepository.save(address);
+        Optional<AddressModel> existingAddress = getAddressById(id);
+        if (existingAddress.isPresent()) {
+            // Replace the old address with the new address
+            address.setId(id);
+            addressList.remove(existingAddress.get());
+            addressList.add(address);
+            return address;
+        }
+        return null;  // Address not found
     }
 
     // Delete an address
     public boolean deleteAddress(Long id) {
-        if (addressRepository.existsById(id)) {
-            addressRepository.deleteById(id);
+        Optional<AddressModel> address = getAddressById(id);
+        if (address.isPresent()) {
+            addressList.remove(address.get());
             return true;
         }
         return false;
